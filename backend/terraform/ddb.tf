1a402a681b9ac08fbf3b20ed2ad562001986ec54
Y-Jacob Mlo <Jacobmlo.2002@gmail.com>
2025-01-22 14:00:53 -0500

    wip

--- backend/terraform/ddb.tf
+++ backend/terraform/ddb.tf
@@ -1,17 +1,20 @@
 resource "aws_dynamodb_table" "projects" {
   name           = "Projects"
   billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "project_id"
-  range_key      = "user_id"
-  attribute {
-    name = "project_id"
-    type = "S"
-  }
+  hash_key       = "user_id"    # Changed hash key to user_id
+  range_key      = "project_id" # Keep project_id as the sort key
+
   attribute {
     name = "user_id"
     type = "S"
   }
+
+  attribute {
+    name = "project_id"
+    type = "S"
+  }
 }
+
 
 resource "aws_dynamodb_table" "tasks" {
   name           = "Tasks"
